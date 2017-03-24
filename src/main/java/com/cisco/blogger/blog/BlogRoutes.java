36a08ffa6accf287b556c09abf0bd01583dafbc6
Sahil Narwal <sanarwal@cisco.com>
2017-03-24 17:38:07 +0530

    module separation

--- src/main/java/com/cisco/blogger/blog/BlogRoutes.java
+++
@@ -1,9 +0,0 @@
-package com.cisco.blogger.blog;
-
-public interface BlogRoutes {
-	
-	String BLOG = "/api/blog";
-	
-	String SPECIFIC_BLOG = BLOG + "/:identifier";
-	
-}
--- src/main/java/com/cisco/blogger/user/UserRoutes.java
+++
@@ -1,7 +0,0 @@
-package com.cisco.blogger.user;
-
-public interface UserRoutes {
-	
-	String USER = "/api/user";
-	
-}
